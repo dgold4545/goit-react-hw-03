dbc7baf28636b5dd6e4000e3acba4db755e69ada
Dmytro Holovskyi <80936904545dgold@gmail.com>
2024-10-08 18:09:12 +0300

    add essential files

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,7 @@
+import styles from './ContactForm.module.css'
+
+export default function ContactForm() {
+  return (
+    <div>ContactForm</div>
+  )
+}
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,5 @@
+import styles from "./ContactList.module.css";
+
+export default function ContactList() {
+  return <div>ContactList</div>;
+}
